0f6a18bbafb1ec2a18407ebec7ccc514f8335f89
m4n50n <josegarciarodriguez89@hotmail.com>
2022-01-10 18:13:38 +0000

    refactor: js

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,34 +39,20 @@ const RestartInput = () => {
     TaskInput.focus();
 }
 
-const RenderTask = (task, index) => {
-    let li = document.createElement("li");
-    li.classList.add("list-group-item", "ps-5", "d-flex", "align-items-center", "justify-content-between");
-
-    /**
-     * Uso stopPropagation() para forzar la captura del evento click sólo en <a> y no en <svg> o <path>
-     * https://developer.mozilla.org/es/docs/Web/API/Event/stopPropagation
-     */
-    let LiContent = `${task}
+const RenderTasks = () => {
+    // Con stopPropagation() forzamos la captura del evento click sólo en <a> y no en <svg> o <path> (https://developer.mozilla.org/es/docs/Web/API/Event/stopPropagation)
+    TasksList.innerHTML = AddedTasks.map((task, index) => `<li class="list-group-item ps-5 d-flex align-items-center justify-content-between">${task}
     <div class="d-flex justify-content-end align-items-center">
         <a title="Delete Task" href="#" onclick="event.stopPropagation(); DeleteTask(${index});" class="delete-task d-lg-none px-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
             </svg>
         </a>
-    </div>`;
-
-    li.innerHTML = LiContent;
-    TasksList.appendChild(li);
+    </div></li>`).join("");
 }
 
 const RenderApp = () => {
-    TasksList.innerHTML = "";
-
-    AddedTasks.forEach((task, index) => {
-        RenderTask(task, index);
-    });
-
+    RenderTasks();
     UpdateTaskCounter();
     RestartInput();
 }
